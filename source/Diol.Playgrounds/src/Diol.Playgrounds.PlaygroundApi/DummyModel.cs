38c0740c45f3fcb4aa618399c464967e55a502b5
Chingiz Olzhabaev <olzhabaev.sh@outlook.com>
2023-06-27 17:14:00 +0200

    init

---
+++ source/Diol.Playgrounds/src/Diol.Playgrounds.PlaygroundApi/DummyModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Diol.Playgrounds.PlaygroundApi
+{
+    public class DummyModel
+    {
+        public int DummyNumber { get; set; }
+
+        public string DummyString { get; set; }
+    }
+}
